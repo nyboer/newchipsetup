658fe31f9326e00b4f2f65da8a779ae19e4b4a8a
nyboer <p@nbor.us>
2016-05-27 11:45:55 -0700

    added the 'addafp.sh' script to the file bundle that gets transferred to chip.

--- keymake.sh
+++ keymake.sh
@@ -1,4 +1,4 @@
 #run on mac terminal
 c=${1:-chip}
 ssh-keygen -t rsa
-scp ~/.ssh/id_rsa.pub keyset.sh rename.sh afpdservice.xml rename_input.sh chip@$c.local:~/
+scp ~/.ssh/id_rsa.pub keyset.sh rename.sh afpdservice.xml rename_input.sh addafp.sh chip@$c.local:~/
