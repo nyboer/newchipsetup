8ffcc3845403fee470890a59167de73a3a948d5a
nyboer <p@nbor.us>
2016-10-24 18:18:12 -0700

    -y for puredata

--- puredata.sh
+++ puredata.sh
@@ -2,4 +2,4 @@
  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 9f0fe587374bbe81
  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key D63D3D09C39F5EEB
  sudo apt-get update
- sudo apt-get install puredata
+ sudo apt-get install -y puredata
