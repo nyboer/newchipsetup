116b304d22040014361375e6a50e924ee3cf5d1c
nyboer <p@nbor.us>
2016-10-24 19:40:17 -0700

    added bin/bash at top!

--- puredata.sh
+++ puredata.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
  sudo add-apt-repository "deb http://apt.puredata.info/releases `lsb_release -c | awk '{print $2}'` main"
  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 9f0fe587374bbe81
  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key D63D3D09C39F5EEB
