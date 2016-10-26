497ada40ef7bf21b09bd615b274ccd6ab5cf470a
nyboer <p@nbor.us>
2016-10-26 15:18:55 -0700

    from git rather than tar

--- cursynth.sh
+++ cursynth.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y autopoint libpulse-dev libncurses5-dev libasound2-dev texinfo
-wget ftp://ftp.gnu.org/gnu/cursynth/cursynth-1.5.tar.gz
-tar -xvf cursynth-1.5.tar.gz
-cd cursynth-1.5
+sudo apt-get update && sudo apt-get install -y autopoint libpulse-dev libncurses5-dev libasound2-dev texinfo git
+git clone https://github.com/mtytel/cursynth.git
+cd cursynth
 autoreconf --force -i && ./configure && make clean &&  make
 sudo make install
