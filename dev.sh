7baa6211e38a81730b2142f20e483bc7c98a7f82
nyboer <p@nbor.us>
2016-12-02 17:28:53 -0800

    added libtool to packages

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt-get  update && sudo apt-get install -y python-setuptools i2c-tools python-dev texinfo libtool-bin build-essential git autoconf x11vnc software-properties-common locales xclip psmisc
+sudo apt-get  update && sudo apt-get install -y python-setuptools i2c-tools python-dev texinfo libtool-bin libtool build-essential git autoconf x11vnc software-properties-common locales xclip psmisc
 sudo easy_install pip
 git clone https://github.com/xtacocorex/CHIP_IO.git
